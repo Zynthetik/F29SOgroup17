1a98b2148a8ef68a258f84d4295d63cb7e2d5960
teganstorm <55839174+teganstorm@users.noreply.github.com>
2021-01-13 15:13:21 +0400

    deleting the search section and adding search bar to every page. changing colour to green

--- channels.js
+++ channels.js
@@ -6,10 +6,6 @@ function openFeed() {
     open("feed.html");
     close("channels.html");
 }
-function openSearch() {    
-    open("search.html");
-    close("channels.html");
-}
 function openChallenges() {    
     open("challenges.html");
     close("channels.html");
@@ -23,5 +19,23 @@ function newChannel() {
     // prompt user for channel name
     // channel tags
     // channel public or private
+    return 1;
+}
+
+var searchfor;
+var newtext;
+function searching() {
+    searchfor = document.getElementById("searchinput").value;
+    if (searchfor != "") {
+        newtext = "Showing results for: "+searchfor+" . . . \n1 result found";
+        document.getElementById("searchresult").innerHTML = newtext;
+        document.getElementById("image1").style.display = "block";
+    }
+    else if (searchfor == "") {
+        newtext = "You didnt enter any key words to search for . . .  ";
+        document.getElementById("searchresult").innerHTML = newtext;
+    }
+    // check with database for match
+    
     return 1;
 }
